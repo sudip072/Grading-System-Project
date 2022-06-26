47a52a76698ea93944ee2f061b09222f421088d2
Sudip Chakma <108189717+sudip072@users.noreply.github.com>
2022-06-26 07:04:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+function Total(){
+  var sub1 = parseInt(document.getElementById("eng").value);
+  var sub2 = parseInt(document.getElementById("mat").value);
+  var sub3 = parseInt(document.getElementById("phy").value);
+  var sub4 = parseInt(document.getElementById("chm").value);
+  var sub5 = parseInt(document.getElementById("cmp").value);
+  
+  if(sub1>100 || sub2>100 || sub3>100 || sub4>100 || sub5>100 )
+  {
+    alert("Please Enter Marks in range of 100");
+  }
+  else {
+    var total= sub1 + sub2 + sub3 + sub4 + sub5;
+    document.getElementById("total").innerHTML = "Programing In Java Marks :"+sub1+"<br> Web Development Project Marks: "+sub2+"<br> Sequential Logic System Marks: "+sub3+"<br> Accounting Theory And Practice Marks: "+sub4+"<br> Operating System Application Marks: "+sub5+"<br> Total Marks: "+total;  
+  }
+}
+function Average(){
+  var sub1 = parseInt(document.getElementById("eng").value);
+  var sub2 = parseInt(document.getElementById("mat").value);
+  var sub3 = parseInt(document.getElementById("phy").value);
+  var sub4 = parseInt(document.getElementById("chm").value);
+  var sub5 = parseInt(document.getElementById("cmp").value);
+    
+  if(sub1>100 || sub2>100 || sub3>100 || sub4>100 || sub5>100 )
+  {
+    alert("Please Enter Marks in range of 100");
+  }
+  else {
+    var total= sub1 + sub2 + sub3 + sub4 + sub5;
+    var avg=total/5;
+    document.getElementById("avg").innerHTML="Your Average marks are: "+avg;
+  }
+}
+  
+  
+function Grade(){
+   var sub1 = parseInt(document.getElementById("eng").value);
+  var sub2 = parseInt(document.getElementById("mat").value);
+  var sub3 = parseInt(document.getElementById("phy").value);
+  var sub4 = parseInt(document.getElementById("chm").value);
+  var sub5 = parseInt(document.getElementById("cmp").value);
+    
+   if(sub1>100 || sub2>100 || sub3>100 || sub4>100 || sub5>100 )
+  {
+    alert("Please Enter Marks in range of 100");
+  }else {
+     var total= sub1 + sub2 + sub3 + sub4 + sub5;
+     var avg=total/5;
+     
+     if(avg>=80 && avg<=100)
+     {
+       document.getElementById("grade").innerHTML="You Got A+ Grade";
+     }
+     else if(avg>=75 && avg<=80)
+     {
+       document.getElementById("grade").innerHTML="You Got A+ Grade";
+     }
+     else if(avg>=70 && avg<=75)
+     {
+       document.getElementById("grade").innerHTML="You Got A Grade";
+     }
+     else if(avg>=65 && avg<=70)
+     {
+       document.getElementById("grade").innerHTML="You Got B Grade";
+     }
+     else if(avg>=50 && avg<=60)
+     {
+       document.getElementById("grade").innerHTML="You Got C Grade";
+     }
+     else if(avg>=40 && avg<=50)
+     {
+       document.getElementById("grade").innerHTML="You Got C Grade";
+     }
+     
+     else {
+       document.getElementById("grade").innerHTML="You Got F Grade";
+     }
+     
+    
+  }
+}
